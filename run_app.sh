217e704c315bbd67294e4af2c9999a67b378c153
soni-ratnesh <ratneshk.99@gmail.com>
2021-10-01 03:09:06 +0530

    Add run fastapi shell file.

---
+++ run_app.sh
@@ -0,0 +1,21 @@
+#!/bin/bash  
+# shell for running fastapi server
+
+echo "Installing pip3"
+sudo apt-get install python3-pip
+
+echo "Configuring Virtual Environemt"
+
+echo "Environment setup"
+echo "Creating Vertual environmet named dev"
+python3 -m venv dev/
+
+echo "Environment active"
+source dev/bin/activate
+
+echo "installing dependencies"
+sudo pip3 install -r requirements.txt
+
+echo "Running server"
+uvicorn main:app --reload
+
