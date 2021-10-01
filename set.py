95350b307285d924648cef85410cf1adf98a027d
Sankalp Mehra <sankalpmehra.mehra@gmail.com>
2021-10-01 15:46:59 +0530

    Update set.py

--- set.py
+++ set.py
@@ -68,6 +68,35 @@ print(my_set)
 my_set.update([4, 5], {1, 6, 8})
 print(my_set)
 
+# Difference between discard() and remove()
+
+# initialize my_set
+my_set = {1, 3, 4, 5, 6}
+print(my_set)
+
+# discard an element
+# Output: {1, 3, 5, 6}
+my_set.discard(4)
+print(my_set)
+
+# remove an element
+# Output: {1, 3, 5}
+my_set.remove(6)
+print(my_set)
+
+# discard an element
+# not present in my_set
+# Output: {1, 3, 5}
+my_set.discard(2)
+print(my_set)
+
+# remove an element
+# not present in my_set
+# you will get an error.
+# Output: KeyError
+
+my_set.remove(2)
+
 
 # Set union method
 # initialize A and B
@@ -77,3 +106,5 @@ B = {4, 5, 6, 7, 8}
 # use | operator
 # Output: {1, 2, 3, 4, 5, 6, 7, 8}
 print(A | B)
+print(A & B)
+print(A - B)
