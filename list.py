3bf30303a1dec6d872fc5565b609451621e75e17
Sankalp Mehra <sankalpmehra.mehra@gmail.com>
2021-10-01 15:29:53 +0530

    Update list.py

--- list.py
+++ list.py
@@ -106,3 +106,35 @@ print(odd)
 odd.extend([9, 11, 13])
 
 print(odd)
+
+
+
+# Concatenating and repeating lists
+odd = [1, 3, 5]
+
+print(odd + [9, 7, 5])
+
+print(["re"] * 3)
+
+my_list = ['p','r','o','b','l','e','m']
+my_list.remove('p')
+
+# Output: ['r', 'o', 'b', 'l', 'e', 'm']
+print(my_list)
+
+# Output: 'o'
+print(my_list.pop(1))
+
+# Output: ['r', 'b', 'l', 'e', 'm']
+print(my_list)
+
+# Output: 'm'
+print(my_list.pop())
+
+# Output: ['r', 'b', 'l', 'e']
+print(my_list)
+
+my_list.clear()
+
+# Output: []
+print(my_list)
